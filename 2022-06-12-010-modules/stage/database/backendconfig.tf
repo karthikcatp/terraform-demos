afaf0bd266a1c8b5728f18376594907ac053d66e
rathinamtrainers <rajan@rathinamtrainers.com>
2022-06-12 14:54:51 +0530

    2022-06-12 Session (Mysql module, webserver integration, module locals, module generalization)

---
+++ 2022-06-12-010-modules/stage/database/backendconfig.tf
@@ -0,0 +1,11 @@
+terraform {
+  # Reference: https://www.terraform.io/language/settings/backends/s3
+  backend "s3" {
+    bucket = "rtc2022q3-terraform-state-1"
+    key = "stage/data-stores/mysql/terraform.tfstate"
+    region = "ap-south-1"
+
+    dynamodb_table = "terrform_locks"
+    encrypt = true
+  }
+}
